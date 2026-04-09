3736216850e52915ade2eea7c449184d799fe34a
urvashi-baria <urvashibaria2004@gmail.com>
2026-04-09 14:47:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (22.33%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -3,7 +3,7 @@ public:
     bool checkString(string s) {
         int countA = 0;
         for(int i = 0;i<s.length();i++) {
-            countA++;
+            if(s[i]=='a'){countA++;}
             if(s[i]=='b' && s[i+1]=='a') return false;  
         }
        if(countA==0) {return true;} 
