a167593b2d3649a77788952306813495652f77a3
urvashi-baria <urvashibaria2004@gmail.com>
2026-04-13 14:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (53.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char> v;
-        for(int i = 0;i<s.length();i++){
-            s[i] = tolower(s[i]);
-            if((s[i]>='a' && s[i]<='z')|| (s[i]>='0' && s[i]<='9')){
-                v.push_back(s[i]);
-            }
-        }
-
-        
+        // vector<char> v;
         // for(int i = 0;i<s.length();i++){
+        //     s[i] = tolower(s[i]);
         //     if((s[i]>='a' && s[i]<='z')|| (s[i]>='0' && s[i]<='9')){
         //         v.push_back(s[i]);
         //     }
         // }
 
-        for(int i = 0;i<v.size()/2;i++){
-            if(v[i]!=v[v.size()-i-1]){
-                return false;
+        
+        // // for(int i = 0;i<s.length();i++){
+        // //     if((s[i]>='a' && s[i]<='z')|| (s[i]>='0' && s[i]<='9')){
+        // //         v.push_back(s[i]);
+        // //     }
+        // // }
+
+        // for(int i = 0;i<v.size()/2;i++){
+        //     if(v[i]!=v[v.size()-i-1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int i  = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+            }
+            else if(!isalnum(s[j])){
+                j--;
+            }
+            else{
+                if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+                }
+                i++;
+                j--;
             }
         }
         return true;
