b5d5bf09ae4eb7eaf7a1fdd8c75ffc86e40efeaf
urvashi-baria <urvashibaria2004@gmail.com>
2026-06-09 14:40:00 +0530

    Time: 68 ms (15.70%) | Memory: 92.9 MB (15.98%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i = 0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto ele:m){
+            if(ele.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
