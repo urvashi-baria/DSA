5b048376a9dc7bee386116d3a7e91dffb28e5aa8
urvashi-baria <urvashibaria2004@gmail.com>
2026-07-02 20:17:49 +0530

    Time: 6 ms (72.51%) | Memory: 11.4 MB (23.77%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -11,21 +11,25 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
+        bool flag = false;
         while(fast!=NULL && fast->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
             if(slow==fast){
+                flag=true;
                 break;
             }
         }
-        if(fast==NULL || fast->next == NULL){
+        if(flag==false){
             return NULL;
         }
-        ListNode* temp = head;
-        while(temp!=slow){
+        else{
+          ListNode* temp = head;
+          while(temp!=slow){
             temp=temp->next;
             slow=slow->next;
         }
         return temp;
+     }
     }
 };
