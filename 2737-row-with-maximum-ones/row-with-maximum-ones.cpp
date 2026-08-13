510dd7918827998fed086397132ca85bbefaf534
urvashi-baria <urvashibaria2004@gmail.com>
2026-08-13 23:57:19 +0530

    Time: 9 ms (17.52%) | Memory: 64.7 MB (39.07%) - LeetSync

---
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int n = mat.size();
+        int m = mat[0].size();
+        int maxCount = INT_MIN;
+        int count = 0;
+        int row = -1;
+        for(int i = 0;i<n;i++){
+            int low = 0;
+            int high = m-1;
+            int first = 0;
+            bool flag = false;
+            sort(mat[i].begin(),mat[i].end());
+            while(low<=high){
+                    int mid = low+(high-low)/2;
+                    if(mat[i][mid]==1){
+                        flag=true;
+                        first=mid;
+                        high=mid-1;
+                    }
+                    else if(mat[i][mid]==0){
+                        low=mid+1;
+                    }
+                    
+            }
+
+            low=0;
+            high=m-1;
+            int last = 0;
+            while(low<=high){
+                    int mid = low+(high-low)/2;
+                    if(mat[i][mid]==1){
+                        last=mid;
+                        low = mid+1;
+                    }
+                    else if(mat[i][mid]==0){
+                        low=mid+1;
+                    }
+            }
+           
+            if(flag==false){
+               count = 0;
+            }
+            else{
+                count=last-first+1;
+            }
+
+            if(maxCount<count){
+               row = i;
+               maxCount = count;
+            } 
+        }
+        
+        ans.push_back(row);
+        ans.push_back(maxCount);
+        return ans;
+
+    }
+};
