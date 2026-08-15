3341ad0661986eef030f5094a937c35a71acebdc
urvashi-baria <urvashibaria2004@gmail.com>
2026-08-15 10:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (49.14%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -9,11 +9,11 @@ public:
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 return mid;
             }
-            else if(arr[mid]<arr[mid+1]){
-                low=mid+1;
+            else if(arr[mid]<arr[mid-1]){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return 100;
