aa93d683d13f32c51869b61e67a9ff4c2a0e6c30
urvashi-baria <urvashibaria2004@gmail.com>
2026-08-16 10:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (67.26%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // int n = arr.size();
+        // multimap<int,int> diffNum;
+        // for(int ele:arr){
+        //     diffNum.insert({abs(ele-x),ele});
+        // }
+        // vector<int> result;
+        // auto ele=diffNum.begin();
+        // for(int i=0;i<k;i++){
+        //     result.push_back(ele->second);
+        //     ele++;
+        // }
+        // sort(result.begin(),result.end());
+        // return result; 
+
         int n = arr.size();
-        multimap<int,int> diffNum;
-        for(int ele:arr){
-            diffNum.insert({abs(ele-x),ele});
+        int start = 0;
+        int end = n-k;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
         }
-        vector<int> result;
-        auto ele=diffNum.begin();
-        for(int i=0;i<k;i++){
-            result.push_back(ele->second);
-            ele++;
-        }
-        sort(result.begin(),result.end());
-        return result;
+
+        vector<int> ans(arr.begin()+start,arr.begin()+start+k);
+        return ans;
+
     }
 };
