d5ff020833ed80198a81ea7dd43cf2f5fd5603f4
urvashi-baria <urvashibaria2004@gmail.com>
2026-09-02 19:40:37 +0530

    Time: 9 ms (44.31%) | Memory: 14.2 MB (58.27%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n= customers.length();
-        int pre[n+1]; //calculate N
-        int suf[n+1]; //calculate Y
-        pre[0] = 0;
+        // int n = customers.length();
+        // int minCount = INT_MAX;
+        // int ans =-1;
+        // int j = 0;
+        // while(j<=n){
+        //     int count = 0;
+        //     for(int i = 0;i<n;i++){
+        //         if(i>=j && customers[i]=='Y'){
+        //             count++;
+        //         }
+        //         else if(i<j && customers[i]=='N'){
+        //             count++;
+        //         }
+        //     }
+        //     if(count<minCount){
+        //         minCount = count;
+        //         ans = j;
+        //     }
+        //     j++;
+        // }
+        // return ans;
+
+        int n = customers.length();
+        int pre[n+1];  //calculate 'N'
+        int suf[n+1];  //calculate 'Y'
+        pre[0]=0;
         for(int i = 0;i<n;i++){
-            int count = 0;
-            if(customers[i]=='N') count++;
-            pre[i+1]=pre[i]+count;
+            pre[i+1]=pre[i]+((customers[i]=='N')?1:0);
         }
-        suf[n] = 0;
+        suf[n]=0;
         for(int i = n-1;i>=0;i--){
-            int count = 0;
-            if(customers[i]=='Y') count++;
-            suf[i] = suf[i+1]+count;
+            suf[i]=suf[i+1]+((customers[i]=='Y')?1:0);
         }
-        int min = n;
+        
+        int minCount = INT_MAX;
         for(int i = 0;i<=n;i++){
-           pre[i]=pre[i]+suf[i];
-           if(pre[i]<min){
-             min = pre[i];
-           }
+            pre[i]+=suf[i];
+            if(pre[i]<minCount){
+               minCount = pre[i];
+            }
         }
 
         for(int i = 0;i<=n;i++){
-            if(min==pre[i]) return i;
+            if(pre[i]==minCount){
+                return i;
+            }
         }
-        return n;
+        return -1;
     }
 };
