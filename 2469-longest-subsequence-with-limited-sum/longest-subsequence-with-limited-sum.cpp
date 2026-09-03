601f65b925da91dba97262ee8bcc0bdc5b9ec3a9
urvashi-baria <urvashibaria2004@gmail.com>
2026-09-03 23:40:17 +0530

    Time: 3 ms (64.02%) | Memory: 18 MB (26.94%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -6,25 +6,26 @@ public:
         //4 5 2 1  = nums 3 10 21  = queries
         sort(nums.begin(),nums.end());
         //1 2 4 5 = nums 
-        vector<int> suff(n);
-        suff[0]=nums[0];
+        vector<int> pref(n);
+        pref[0]=nums[0];
         for(int i = 1;i<n;i++){
-            suff[i]=suff[i-1]+nums[i];
+            pref[i]=pref[i-1]+nums[i];
         }
         //1 3 7 12
         vector<int> ans(m);
         for(int i = 0;i<m;i++){
-            int count = 0;
-            for(int j = 0;j<n;j++){
-                if(suff[j]<queries[i]){
-                    count++;
-                }
-                else if(suff[j]==queries[i]){
-                    count++;
-                    break;
+            int count=0;
+            int low = 0;
+            int high = n-1;
+            while(low<=high){
+                int mid = low+(high-low)/2;
+                if(pref[mid]>queries[i]){
+                    high=mid-1;
                 }
                 else{
-                    break;
+                    //pref[mid]<=queries[i]
+                    count=mid+1;
+                    low = mid+1;
                 }
             }
             ans[i]=count;
