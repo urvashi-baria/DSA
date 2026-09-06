3a3e1dc0c423f57baac720076c055959cf181062
urvashi-baria <urvashibaria2004@gmail.com>
2026-09-06 08:40:37 +0530

    Time: 4 ms (69.75%) | Memory: 12.9 MB (73.64%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        int n = digits.size();
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(auto ele:digits){
+            m[ele]++;
+        }
+        for(int i=100;i<=999;i+=2){
+            int x = i;
+            int a = x%10;
+            x=x/10;
+            int b = x%10;
+            x=x/10;
+            int c = x%10;
+            x=x/10;
+            if(m.find(a)!=m.end()){
+                m[a]--;
+                if(m[a]==0){
+                    m.erase(a);
+                }
+                if(m.find(b)!=m.end()){
+                    m[b]--;
+                    if(m[b]==0){
+                        m.erase(b);
+                    }
+                    if(m.find(c)!=m.end()){
+                        ans.push_back(i);
+                    }
+                m[b]++;   
+                } 
+                m[a]++;
+            }
+        }
+        return ans;
+    }
+};
